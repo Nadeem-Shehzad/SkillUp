189bb9690d2a79df7f8ff7e048188a82a492fca3
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-10-29 08:18:36 +0500

    converting project to MS

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,10 +2,10 @@ import Redis from 'ioredis';
 import { logger } from "@skillup/common-utils";
 
 const redis = new Redis({
-  host: 'redis-19673.c100.us-east-1-4.ec2.redns.redis-cloud.com',
-  port: 19673,
+  host: 'redis-15448.c82.us-east-1-2.ec2.redns.redis-cloud.com',
+  port: 15448,
   username: 'default',
-  password: '0Fb87gwAZHrp4lSj4FZtnyQU4gxPE1oj',
+  password: 'a4UjzshlvRi1emtDUyDIGeDdc5cn4Q0R',
   maxRetriesPerRequest: null
 });
 
